8382443faca0f68856d8d4b48952f5ef67b584a1
Freak <131407675+FreakMediaLP@users.noreply.github.com>
2025-10-03 17:20:05 +0200

    fixed revert

--- docker/start.sh
+++ docker/start.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
+# Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Start the Next.js backend in the background on port 4000
+# Start the Next.js backend server in the background on port 4000
+# (for internal use by Nginx)
 npm start -- -p 4000 &
 
-# Start Nginx in the foreground to keep the container running
+# Start Nginx in the foreground. This will keep the container running.
+# The 'daemon off;' directive prevents Nginx from forking to the background.
 exec nginx -g 'daemon off;'
