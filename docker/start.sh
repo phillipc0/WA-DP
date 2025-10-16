4f5b78623e453fb17f874755ef3b59f0fac9784b
Freak <131407675+FreakMediaLP@users.noreply.github.com>
2025-10-16 14:41:32 +0200

    testing fix

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 set -e
 
+BACKEND_PFJ="/app/data/portfolio.json"
+FRONTEND_PFJ="/app/frontend/portfolio.json"
+
+# Check if the source file exists / a volume was mounted
+if [ -f "$BACKEND_PFJ" ]; then
+    cp "$BACKEND_PFJ" "$FRONTEND_PFJ"
+fi
+
 # Start the Next.js backend server in the background
 npm start -- -p 4000 &
 
